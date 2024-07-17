51ebd9a12a64e471d36ce9e7e3ec542305ccec7c
Mr-Sena <jsenna985@gmail.com>
2024-07-17 12:11:25 -0300

    Acesso de referências.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,15 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //Spring Cloud config import.
+    implementation("org.springframework.cloud:spring-cloud-config-server")
 }
+
+ext {
+    set("springCloudVersion", "2020.0.3")
+}
+
 
 tasks.test {
     useJUnitPlatform()
